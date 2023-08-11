df88d28d7aed30cbf5ea9aea4259a70a1f160ae1
abbiedp <abbiedp@ymail.com>
2023-08-11 14:45:47 +0700

    menambah midleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,12 +3,13 @@ import { signup, login } from "../controller/userController.js"
 
 
 import { validateSignup, validateSignupSocial } from "../middlewares/validateInput.js";
-import { verifyOtp } from "../middlewares/verifyInput.js";
+import { verifyOtp, verifyUserNotExist } from "../middlewares/verifyInput.js";
 
 const router = express.Router();
 
-router.post("/signup", validateSignup, verifyOtp, signup);
+router.post("/signup", validateSignup, verifyUserNotExist, verifyOtp, signup);
 router.post("/login", login);
-router.post("/social", validateSignupSocial, signup);
+router.post("/signup_social", validateSignupSocial,verifyUserNotExist, signup);
+router.post("/login_social", login);
 
 export default router;
